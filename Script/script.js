2228f34cd2762b2f2041f1bbb1c97450ecfff9e1
Dmitri Larin <Diman-Larin@mail.ru>
2021-01-28 17:53:14 +0400

    Первые изменения

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,14 @@
-document.getElementById('github').onclick = function() {
+
+/*document.getElementById('github').onclick = function() {
     console.log('Изучил git и github');
-};
+};*/
+let money
+let income
+let addExpencess
+let deposit
+let mission
+let period
+
+alert('Hello');
+
+console.log('Слово');
