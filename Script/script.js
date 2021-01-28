d14385e648c28894830bb6e33f92c4ecd62398a6
Dmitri Larin <Diman-Larin@mail.ru>
2021-01-28 19:04:25 +0400

    Изменил период

--- Script/script.js
+++ Script/script.js
@@ -2,12 +2,12 @@
 /*document.getElementById('github').onclick = function() {
     console.log('Изучил git и github');
 };*/
-let money
-let income
-let addExpencess
-let deposit
-let mission
-let period
+let money = 100;
+let income = 120;
+let addExpencess = 50;
+let deposit = 40;
+let mission = 500;
+let period = 6;
 
 alert('Hello');
 
