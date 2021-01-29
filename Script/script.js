80240175a817d9acb2b45435b7b9c66eeeaeff80
Dmitri Larin <Diman-Larin@mail.ru>
2021-01-29 16:56:27 +0400

    Проверка

--- Script/script.js
+++ Script/script.js
@@ -3,11 +3,21 @@
     console.log('Изучил git и github');
 };*/
 let money = 1500;
-let income = 500;
-let addExpencess = 'internet, taxi, purchases';
+let income = 'freelance';
+let addExpencess = ('internet, taxi, purchases');
 let deposit = true;
 let mission = 5000;
 let period = 4;
 
 
-console.log('Слово в консоли');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpencess.length);
+console.log('Период равен ' + period + ' месяца');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpencess.toLowerCase().split(', '));
+
+//console.log(money / 30);
+let budgetDay = money / 30;
+console.log(budgetDay);
