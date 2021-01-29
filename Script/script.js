00f24264fa81d172290a24d559744293eeae5d6e
Dmitri Larin <Diman-Larin@mail.ru>
2021-01-29 16:50:18 +0400

    Выполнил второе ДЗ

--- Script/script.js
+++ Script/script.js
@@ -2,13 +2,12 @@
 /*document.getElementById('github').onclick = function() {
     console.log('Изучил git и github');
 };*/
-let money = 100;
-let income = 120;
-let addExpencess = 50;
-let deposit = 40;
-let mission = 500;
-let period = 6;
+let money = 1500;
+let income = 500;
+let addExpencess = 'internet, taxi, purchases';
+let deposit = true;
+let mission = 5000;
+let period = 4;
 
-alert('Hello');
 
 console.log('Слово в консоли');
