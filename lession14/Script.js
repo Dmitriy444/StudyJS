ad9cf1272d32e36b7d1447821c1a051707495edc
Dmitri Larin <Diman-Larin@mail.ru>
2021-02-21 23:01:02 +0400

    проверка

--- lession14/Script.js
+++ lession14/Script.js
@@ -6,38 +6,63 @@ function DomElement (selector, height, width, bg, fontSize) {
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    
+    //let elementDiv = new DomElement('.new-field', '60px', '300px', '#C0C0C0', '25px');
+    //let newField = document.querySelector('.new-field');
+    //document.querySelector('.new-field').style.cssText = `height: this.height;
+    //width: this.width;
+    //background-color: this.bg;
+    //font-size: this.fontSize;
+    //`; 
 }
+ //let elementDiv = new DomElement('.block', '60px', '300px', '#C0C0C0', '20px');
+    //document.querySelector('.block').style.cssText = `height: elementDiv.height;
+    //width: elementDiv.width;
+    //background-color: elementDiv.bg;
+    //font-size: elementDiv.fontSize;
+    //`;
 
-DomElement.prototype.newMethod = function(){
-    //prompt('enter');
+ let elementDiv = new DomElement('.new-field', '60px', '300px', '#C0C0C0', '25px');
+    document.querySelector('.new-field').style.height = elementDiv.height;
+    document.querySelector('.new-field').style.width = elementDiv.width;
+    document.querySelector('.new-field').style.background = elementDiv.bg;
+    document.querySelector('.new-field').style.fontSize = elementDiv.fontSize;
+
+ let elementId = new DomElement('#idd', '50px', '250px', '#f07a7a', '25px');
+    console.log(elementId);
+    //elementId.newMethod();
+    document.querySelector('#idd').style.height = elementId.height;
+    document.querySelector('#idd').style.width = elementId.width;
+    document.querySelector('#idd').style.background = elementId.bg;
+    document.querySelector('#idd').style.fontSize = elementId.fontSize;
+
+ DomElement.prototype.newMethod = function(){
+    this.selector = prompt('enter');
     if(this.selector[0] === '.'){
-        document.body.insertAdjacentHTML('beforeend', '<div class="block">'  + this.selector.slice(1) + '</div>');
+        document.querySelector('.new-field').insertAdjacentHTML('afterbegin', '<div class="block">'  + this.selector.slice(1) + '</div>');
     } else if(this.selector[0] === '#'){
-        document.body.insertAdjacentHTML('beforeend', '<p id="idd">' + this.selector.slice(1) + '</p>');
+        document.querySelector('p').insertAdjacentHTML('afterbegin', '<p id="idd">' + this.selector.slice(1) + '</p>');
+    } else {
+        return this.newMethod();
     }
+
 };
-let elementDiv = new DomElement('.block', '60px', '300px', '#C0C0C0', '20px');
-console.log(this.elementDiv);
-elementDiv.newMethod();
-//elementDiv.createElement('div');
-document.querySelector('.block').style.height = this.height + 'px'; width =  this.width + 'px'; background-color = this.bg; font-size = this.fontSize + 'px';
-/*
-elem.style.cssText = `height: elementDiv.height;
-width: elementDiv.width;
-background-color: elementDiv.bg;
-font-size: elementDiv.fontSize;
-`;/*
-elem.style.cssText = `height: elementDiv.height; width: elementDiv.width; background-color: elementDiv.bg; font-size: elementDiv.fontSize;`;
-*/
+DomElement.prototype.newMethod();
 
-let elementId = new DomElement('#idd', '50px', '250px', '#BC8F8F', '25px');
-console.log(elementId);
-elementId.newMethod();
 
-document.querySelector('#idd').style.height = elementId.height;
-document.querySelector('#idd').style.width = elementId.width;
-document.querySelector('#idd').style.background = elementId.bg;
-document.querySelector('#idd').style.fontSize = elementId.fontSize;
+//console.log(car1);
+
+//let elementDiv = new DomElement('.block', '60px', '300px', '#C0C0C0', '20px');
+//сonsole.log(elementDiv);
+//elementDiv.newMethod();
+
+
+
+
+
+
+
+
 
 
 
