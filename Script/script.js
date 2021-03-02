13521f5741d01e5d4c3a1deb11fa221925782f3f
Dmitri Larin <Diman-Larin@mail.ru>
2021-03-02 21:19:57 +0400

    19ДЗ выполнено

--- Script/script.js
+++ Script/script.js
@@ -16,6 +16,9 @@ window.addEventListener('DOMContentLoaded', function(){
                     hours = Math.floor(timeRemaining / 60 / 60);
                     return {timeRemaining, hours, minutes, seconds};
             }
+            //function clearTimer(){
+            //    clearInterval(idInterval);
+            //}
             function updateClock(){
                 let timer = getTimeRemaining();
 
@@ -24,7 +27,6 @@ window.addEventListener('DOMContentLoaded', function(){
                     timerMinutes.textContent = timer.minutes;
                     timerSeconds.textContent = timer.seconds;
                 } else if (timer.timeRemaining <= 0){
-                    // Переделать
                     clearInterval(idInterval);
                     timerHours.textContent = 0;
                     timerMinutes.textContent = 0;
@@ -42,10 +44,10 @@ window.addEventListener('DOMContentLoaded', function(){
                     timerSeconds.textContent = '0' + timerSeconds.textContent;
                 }
             }
-            let idInterval = setInterval(updateClock, 1000);        
+            let idInterval = setInterval(updateClock, 1000);       
     }
     
-    countTimer('2 march 2021');
+    countTimer('3 march 2021');
 
     // Menu
     const toggleMenu = () => {
@@ -53,96 +55,14 @@ window.addEventListener('DOMContentLoaded', function(){
             menu = document.querySelector('menu'),
             closeBtn = document.querySelector('.close-btn'),
             menuItems = document.querySelectorAll('ul>li');
-            let count = 0;
-            let flyInterval;
-            let animate = false;
 
         const actionMenu = () => {
-            
-            let menuLeft = function(){
-                flyInterval = requestAnimationFrame(menuLeft);
-                count++;
-                if(count < 96){
-                    menu.style.left = count * 10 + 'px';
-                } else{
-                    cancelAnimationFrame(flyInterval);
-                }
-                console.log(count);
-            };  
-            /*
-            let menuLeft = function(){
-                count++;
-                menu.style.left = count * 95 + 'px';
-                if(count < 10 && screen.width > 768){
-                    setTimeout(menuLeft, 20);
-                } 
-            };
-            */
-            /*
-            let menuRight = function(){
-                count--;
-                menu.style.left = count + 'px';
-                if(count > 0 && screen.width > 768){
-                    setTimeout(menuRight);
-                }
-            };
-            */
-           btnMenu.addEventListener('click', function(){
-            if(!menu.style.transform || menu.style.transform === `translate(-100%)` &&  menu.style.left === `0` || !animate){
-                menu.style.transform = `translate(0)`;
-                flyInterval = requestAnimationFrame(menuLeft);
-                animate = true;
-            } else{
-                animate = false;
-                menu.style.left = 0 + 'px';
-                menu.style.transform = `translate(-100%)`;
-                cancelAnimationFrame(flyInterval);
-
-                //flyInterval = requestAnimationFrame(flyAnimate);
-                //count = 0 + 'px';
-                //menu.style.left = 0 + 'px';
-                //menuRight();
-            }  
-           });
-           
-            closeBtn.addEventListener('click', function(){
-                if(!menu.style.transform || menu.style.transform === `translate(-100%)` &&  menu.style.left === `0` || !animate){
-                    menu.style.transform = `translate(0)`;
-                    flyInterval = requestAnimationFrame(menuLeft);
-                    animate = true;
-                } else{
-                    animate = false;
-                    menu.style.left = 0 + 'px';
-                    menu.style.transform = `translate(-100%)`;
-                    cancelAnimationFrame(flyInterval);
-
-                    //flyInterval = requestAnimationFrame(flyAnimate);
-                }
-            });
-            menuItems.forEach((elem) => elem.addEventListener('click', function(){
-                if(!menu.style.transform || menu.style.transform === `translate(-100%)` &&  menu.style.left === `0` || !animate){
-                    menu.style.transform = `translate(0)`;
-                    flyInterval = requestAnimationFrame(menuLeft);
-                    animate = true;
-                } else{
-                    animate = false;
-                    menu.style.left = 0 + 'px';
-                    menu.style.transform = `translate(-100%)`;
-                    cancelAnimationFrame(flyInterval);
-                    
-                    //flyInterval = requestAnimationFrame(flyAnimate);
-                }
-            }));
-            
+            menu.classList.toggle('active-menu');
         };
-        actionMenu();
-
-        //console.log(menu.getBoundingClientRect());
-        //btnMenu.addEventListener('click', actionMenu);
-        //closeBtn.addEventListener('click', actionMenu);
-
-        //menuItems.forEach((elem) => elem.addEventListener('click', actionMenu));
-        
+        console.log(menu.getBoundingClientRect());
+        btnMenu.addEventListener('click', actionMenu);
+        closeBtn.addEventListener('click', actionMenu);
+        menuItems.forEach((elem) => elem.addEventListener('click', actionMenu));
     };
 
     toggleMenu();
@@ -151,15 +71,36 @@ window.addEventListener('DOMContentLoaded', function(){
     const togglePopup = () => {
         const popup = document.querySelector('.popup'),
         popupBtn = document.querySelectorAll('.popup-btn'),
-        popupClose = document.querySelector('.popup-close');
+        popupClose = document.querySelector('.popup-close'),
+        popupContent = document.querySelector('.popup-content');
 
+    let count = 0;
+    
+    let popupDown = function(){
+        count++;
+        if(count < 60 && screen.width > 768){
+            popupContent.style.top = count + 'px';
+            setTimeout(popupDown);
+        }
+    };
+    
+    let popupUp = function(){
+        count--;
+        if(count > 0){
+            popupContent.style.top = count + 'px';
+            setTimeout(popupUp);
+        }
+    };  
+    
         popupBtn.forEach((elem) => {
             elem.addEventListener('click', () => {
                 popup.style.display = 'block';
+                popupDown();
             });
         });
         popupClose.addEventListener('click', () => {
             popup.style.display = 'none';
+            popupUp();
         });
     };
     togglePopup();
